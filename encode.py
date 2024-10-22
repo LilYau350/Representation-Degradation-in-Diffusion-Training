64d7436f0d23f70216cbcbcad5d5644cc92f9359
ZPyAo <121719459+LilYau350@users.noreply.github.com>
2024-10-22 14:28:49 +0800

    Update encode.py

--- encode.py
+++ encode.py
@@ -8,6 +8,8 @@ import torchvision.datasets as datasets
 from torch.utils.data import DataLoader
 import numpy as np
 import h5py
+import math
+import random
 from PIL import Image, PngImagePlugin, ImageFile
 
 Image.MAX_IMAGE_PIXELS = None
@@ -22,6 +24,50 @@ ImageNet.h5
 └── val_labels     # Shape: (num_val_samples,)
 '''
 
+def center_crop_arr(pil_image, image_size):
+    if pil_image.size == (image_size, image_size):
+        return np.array(pil_image)
+
+    while min(*pil_image.size) >= 2 * image_size:
+        pil_image = pil_image.resize(
+            tuple(x // 2 for x in pil_image.size), resample=Image.BOX
+        )
+
+    scale = image_size / min(*pil_image.size)
+    pil_image = pil_image.resize(
+        tuple(round(x * scale) for x in pil_image.size), resample=Image.BICUBIC
+    )
+
+    arr = np.array(pil_image)
+    crop_y = (arr.shape[0] - image_size) // 2
+    crop_x = (arr.shape[1] - image_size) // 2
+    return arr[crop_y: crop_y + image_size, crop_x: crop_x + image_size]
+
+
+def random_crop_arr(pil_image, image_size, min_crop_frac=0.8, max_crop_frac=1.0):
+    if pil_image.size == (image_size, image_size):
+        return np.array(pil_image)
+
+    min_smaller_dim_size = math.ceil(image_size / max_crop_frac)
+    max_smaller_dim_size = math.ceil(image_size / min_crop_frac)
+    smaller_dim_size = random.randrange(min_smaller_dim_size, max_smaller_dim_size + 1)
+
+    # Downsample if necessary
+    while min(*pil_image.size) >= 2 * smaller_dim_size:
+        pil_image = pil_image.resize(
+            tuple(x // 2 for x in pil_image.size), resample=Image.BOX
+        )
+
+    scale = smaller_dim_size / min(*pil_image.size)
+    pil_image = pil_image.resize(
+        tuple(round(x * scale) for x in pil_image.size), resample=Image.BICUBIC
+    )
+
+    arr = np.array(pil_image)
+    crop_y = random.randrange(arr.shape[0] - image_size + 1)
+    crop_x = random.randrange(arr.shape[1] - image_size + 1)
+    return arr[crop_y: crop_y + image_size, crop_x: crop_x + image_size]
+
 # Load the AutoencoderKL model
 def initialize_vae():
     vae = AutoencoderKL.from_pretrained("stabilityai/sd-vae-ft-mse").cuda()
@@ -30,7 +76,7 @@ def initialize_vae():
 
 def load_imagenet(input, image_size, batch_size):
     transform = transforms.Compose([
-        transforms.Resize(image_size),
+        transforms.Lambda(lambda img: center_crop_arr(img, image_size)),
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
     ])
